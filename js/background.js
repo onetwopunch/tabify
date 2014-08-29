016834860c2f3d2f8ba7adae5dcea846baaba93b
Ryan Canty <rcanty@qti.qualcomm.com>
2014-08-29 09:56:44 -0700

    seems to be working

--- js/background.js
+++ js/background.js
@@ -1,19 +1,12 @@
 $(document).ready(function () {
-	
-	bind_redirect = function() {
-		var dont_redirect_anymore = false;
-		chrome.tabs.onCreated.addListener(function(tab) {
-			console.log(tab.url)
-			if(tab.url == 'chrome://newtab/') {
-				var url = localStorage.tabify_url;
-				if(!url)
-					url = "chrome://apps";
-				chrome.tabs.update({url: url});
-				dont_redirect_anymore = true;
-				bind_redirect();
-			}
-		});
-	}
-	bind_redirect();
+	chrome.tabs.onCreated.addListener(function(tab) {
+		console.log(tab.url)
+		if(tab.url == 'chrome://newtab/' && localStorage.tabify_power == 'on') {
+			var url = localStorage.tabify_url;
+			if(!url)
+				url = "chrome://apps";
+			chrome.tabs.update({url: url});
+		}
+	});
 });
 
--- js/popup.js
+++ js/popup.js
@@ -4,16 +4,85 @@ $(document).ready(function() {
 		$('#tabify-url').val(localStorage.tabify_url);
 	}
 	
-	$('#tabify-url').keyup(function(event) {
-		if(event.keyCode ==13) {
-			var url = $(this).val();
-			localStorage.tabify_url = url;
-			chrome.tabs.executeScript({
-				code: 'alert("Now when you open a new tab, " + localStorage.tabify_url + " will open.");'
-			});	
-			chrome.tabs.update({ active: true });
+	if (!localStorage.tabify_power) {
+		localStorage.tabify_power = 'on';
+	} 
+	if (localStorage.tabify_power == 'on') {
+		$('#power-text').html(localStorage.tabify_power);
+		$('#tabify-power').removeClass('btn-danger')
+		$('#tabify-power').addClass('btn-success');
+
+	} else {
+		$('#power-text').html(localStorage.tabify_power);
+		$('#tabify-power').removeClass('btn-success')
+		$('#tabify-power').addClass('btn-danger');
+	}
+
+
+	show_dialog = function() {
+		chrome.windows.create({
+	        url: '../dialog.html',
+	        width: 300,
+	        height: 200,
+	        type: 'popup',
+			top: (screen.height / 2) - 100,
+			left: (screen.width / 2) - 150
+		    });
+	}
+    
+	start_with = function(string, substr) {
+		return string.substring(0,substr.length) == substr;
+	}
+	add_http = function(text) {
+		if( !start_with(text, "http") && !start_with(text, 'chrome')) {
+			return 'http://' + text;
+		} else {
+			return text;
 		}
+	}
+
+	set_url = function(elem) {
+		var url = $(elem).val();
+		localStorage.tabify_url = add_http(url);
+		show_dialog();
+	}
+
+
+	$('#tabify-url').keyup(function(event) {
+		if(event.keyCode ==13)
+			set_url(this);
 	});
 
+	$('#tabify-set').click(function() {
+		set_url($('#tabify-url'));
+	});
+
+	$('#tabify-reset').click(function() {
+		var url = $('#tabify-url'); 	
+	   	url.val("chrome://apps");
+		set_url(url); 
+	});
+
+	$('#dialog-close').click(function() {
+		chrome.windows.getLastFocused(function(win) {
+			chrome.windows.remove(win.id);
+		});
+	});
+
+	$('#tabify-power').click(function() {
+		if(localStorage.tabify_power == 'on') {
+			$(this).removeClass('btn-success');
+			$(this).addClass('btn-danger');
+			$('#power-text').html('off');
+			localStorage.tabify_power = 'off';
+		} else {
+			console.log('this');
+			$(this).removeClass('btn-danger');
+			$(this).addClass('btn-success');
+			$('#power-text').html('on');
+			localStorage.tabify_power = 'on';
+		}
+		  
+	});
 });	
 
